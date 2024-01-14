0cb98e9364e596ef5db3b7a4c4de170bd7f64d46
Bruno Bernardes <brunosbernardes@proton.me>
2024-01-14 13:56:28 -0300

    feat: add BirthdayCakeCandles.cs exercise

---
+++ exercises/algorithms/BirthdayCakeCandles.cs
@@ -0,0 +1,13 @@
+static int BirthdayCakeCandles(List<int> candles)
+{
+    var maxHeight = candles.Max();
+    var count = candles.Count(c => c == maxHeight);
+    return count;
+}
+
+
+List<int> candles = [3, 2, 1, 3];
+
+int result = BirthdayCakeCandles(candles);
+
+Console.WriteLine(result);
--- exercises/algorithms/Staircase.cs
+++ exercises/algorithms/Staircase.cs
@@ -1,22 +1,22 @@
-static void Staircase(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            Console.Write(" ");
-        }
-
-        for (int k = 0; k < i; k++)
-        {
-            Console.Write("#");
-        }
-
-        Console.WriteLine();
-    }
-}
-
-
-int n = 6;
-
-Staircase(n);
+// static void Staircase(int n)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 0; j < n - i; j++)
+//         {
+//             Console.Write(" ");
+//         }
+//
+//         for (int k = 0; k < i; k++)
+//         {
+//             Console.Write("#");
+//         }
+//
+//         Console.WriteLine();
+//     }
+// }
+//
+//
+// int n = 6;
+//
+// Staircase(n);
