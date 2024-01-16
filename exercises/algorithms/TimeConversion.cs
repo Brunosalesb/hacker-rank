82cdec67e0b1db37c814c1cc82d6bef17bc495be
Bruno Bernardes <brunosb.8zpei@slmail.me>
2024-01-15 21:49:28 -0300

    feature:add gradientStudents exercise

--- exercises/algorithms/TimeConversion.cs
+++ exercises/algorithms/TimeConversion.cs
@@ -1,12 +1,12 @@
-static string timeConversion(string s)
-{
-    var datetime = DateTime.Parse(s);
-    return datetime.ToString("HH:mm:ss");
-}
-
-
-string s = "07:05:45PM";
-
-string result = timeConversion(s);
-
-Console.WriteLine(result);
+// static string timeConversion(string s)
+// {
+//     var datetime = DateTime.Parse(s);
+//     return datetime.ToString("HH:mm:ss");
+// }
+//
+//
+// string s = "07:05:45PM";
+//
+// string result = timeConversion(s);
+//
+// Console.WriteLine(result);
