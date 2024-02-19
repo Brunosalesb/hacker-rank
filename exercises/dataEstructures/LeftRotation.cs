4811e7b389b4ba9ae5d83c654a979b125cea943c
Bruno Bernardes <brunosb.8zpei@slmail.me>
2024-02-19 20:01:05 -0300

    feat: add LinkedLists.cs exercise

--- exercises/dataEstructures/LeftRotation.cs
+++ exercises/dataEstructures/LeftRotation.cs
@@ -1,13 +1,13 @@
-static List<int> RotateLeft(int d, List<int> arr)
-{
-    var numbers = arr.Take(d);
-    var newArr = arr.Skip(d).ToList();
-    newArr.AddRange(numbers);
-    return newArr;
-}
-
-
-int d = 2;
-List<int> arr = [1, 2, 3, 4, 5];
-List<int> result = RotateLeft(d, arr);
-Console.WriteLine(String.Join(" ", result));
+// static List<int> RotateLeft(int d, List<int> arr)
+// {
+//     var numbers = arr.Take(d);
+//     var newArr = arr.Skip(d).ToList();
+//     newArr.AddRange(numbers);
+//     return newArr;
+// }
+//
+//
+// int d = 2;
+// List<int> arr = [1, 2, 3, 4, 5];
+// List<int> result = RotateLeft(d, arr);
+// Console.WriteLine(String.Join(" ", result));
